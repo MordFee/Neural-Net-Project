04d689c1b1e84534382b6209757bfff60b3cd2f2
MordFee <alfred.bourely@hotmail.fr>
2015-09-19 15:27:55 -0400

    Added backward gradient for learning. Added run functions with changed weight Bugs fixed

--- Multi Layer Network/MultiLayer.py
+++ Multi Layer Network/MultiLayer.py
@@ -2,43 +2,95 @@ from Layer import *
 from utils import *
 from numpy.linalg import norm as norm
 from numpy.random import normal as normal
+from numpy import sum as sum
 
 class MultiLayer:
     """
-    nummberOfNeurons = [nb neurons in input, nb neurons in hidden layer, ... , nb neuron in output]
+    nummberOfNeurons = [nb neurons in input, nb neurons in hidden layer 1, ... , nb neuron in output]
     activationFunction = the activation function used in the network
+    firstLayerNumber or l refers to the layer number. 0 is the input layer, L is the output layer
     """
 
-    def __init__(self, nummberOfNeurons, activationFunction, bLinksFillFunction=normal(0,1), wLinksFillFunction=normal(0,1)):
+    def __init__(self, nummberOfNeurons, activationFunction, bFillFunction=normal(0,1), wFillFunction=normal(0,1)):
         self.numberOfLayer = len(nummberOfNeurons)
-        self.numberOfNeuron = nummberOfNeurons
+        self.numberOfNeurons = nummberOfNeurons
+        self.totalNumberOfNeuron = sum(nummberOfNeurons)
         self.activationFunction = activationFunction
-        self.linkers = []
-        for i in xrange(1, self.numberOfLayer):
-            self.linkers.append(Layer(self.numberOfNeuron[i-1],self.numberOfNeuron[i] , bLinksFillFunction, wLinksFillFunction))
+        self.layers = []
+        for l in xrange(self.numberOfLayer - 1):
+            self.layers.append(Layer(self.numberOfNeurons[l], self.numberOfNeurons[l+1], activationFunction, bFillFunction, wFillFunction))
 
     def run(self, networkInput):
+        """
+        Run the whole network.
+        Takes networkInput of the size of the first layer as input vector.
+        Returns networkOutput of the size of the last layer as output vector
+        """
+        currentInput = networkInput
         currentOutput = []
-        for i in xrange(self.numberOfLayer - 1):
-            currentOutput = self.linkers[i].run_layer(networkInput)
-            networkInput = currentOutput
-        return currentOutput
+        for l in xrange(self.numberOfLayer - 1):
+            currentOutput = self.layers[l].run_layer(currentInput)
+            currentInput = currentOutput
+        networkOutput = currentOutput
+        return networkOutput
 
-    def run_with_changed_value(self, networkInput, linkerNumber, firstNeuronNumber, secondNeuronNumber):
+    def run_with_changed_w(self, networkInput, firstLayerNumber, firstNeuronNumber, secondNeuronNumber, addedWeight):
+        """
+        Run the network with layer[l].w[secondNeuronNumber][firstNeuronNumber] += addedWeight
+        """
+        currentInput = networkInput
         currentOutput = []
-        for i in xrange(self.numberOfLayer - 1):
-            pass #TODO
-        return currentOutput
+        for l in xrange(self.numberOfLayer - 1):
+            if l != firstLayerNumber:
+                currentOutput = self.layers[l].run_layer(currentInput)
+                currentInput = currentOutput
+            else:
+                currentOutput = self.layers[l].run_layer_with_changed_w(currentInput, firstNeuronNumber, secondNeuronNumber, addedWeight)
+                currentInput = currentOutput
+        networkOutput = currentOutput
+        return networkOutput
 
-    def run_and_learn(self, networkInput, wantedOutput, eta, delta):
+    def run_with_changed_b(self, networkInput, firstLayerNumber, secondNeuronNumber, addedWeight):
+        """
+        Run the network with layer[l].b[secondNeuronNumber] += addedWeight
+        """
+        currentInput = networkInput
+        currentOutput = []
+        for l in xrange(self.numberOfLayer - 1):
+            if l != firstLayerNumber:
+                currentOutput = self.layers[l].run_layer(currentInput)
+                currentInput = currentOutput
+            else:
+                currentOutput = self.layers[l].run_layer_with_changed_b(currentInput, secondNeuronNumber, addedWeight)
+                currentInput = currentOutput
+        networkOutput = currentOutput
+        return networkOutput
+
+    def learn_by_backpropagation(self, networkInput, wantedOutput, eta, delta):
+        """
+        eta is the parameter for the descent.
+            If eta is too small, the learning rate will be small.
+            if eta is too big, we might nerver find the minimum.
+        delta is the closeness between the networkOutput and wantedOutput.
+            If delta is too small, we will be overfiting.
+            If delta is too big, the network won't be efficient.
+        """
         currentOutput = self.run(networkInput)
-        diff = norm( map(addition, currentOutput, wantedOutput))
+        diff = norm( map(substraction, currentOutput, wantedOutput))
         cpt = 0
-        while diff > delta and cpt < 1000:
+        while diff > delta and cpt < 100:
             cpt += 1
-            currentOutput2 = currentOutput
+            for l in xrange(self.numberOfLayer - 1, -1, -1): # For back propagation
+                for j in xrange(len(self.layers[l+1])):
+                    for i in xrange(len(self.layers[l])):
+                        currentOutput = self.run(networkInput)
+                        currentOutput2 = self.run_with_changed_w(networkInput, l, i, j, eta)
+                        self.layers[l].w[j][i] -=  (currentOutput2 - currentOutput )
+                    currentOutput = self.run(networkInput)
+                    currentOutput2 = self.run_with_changed_b(networkInput, l, j, eta)
+                    self.layers[l].b[j] -=  (currentOutput2 - currentOutput )
             currentOutput = self.run(networkInput)
-            #TODO
+            diff = norm( map(substraction, currentOutput, wantedOutput))
         return
 
 if __name__ == "__main__":
@@ -46,3 +98,4 @@ if __name__ == "__main__":
     myInput = []
     myNeurons = [10,6,10]
     myNetwork = MultiLayer(myNeurons, sigmoid)
+
