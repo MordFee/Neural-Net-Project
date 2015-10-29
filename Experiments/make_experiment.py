19b661c428c199d74891f70688e527c66f04dfff
MordFee <alfred.bourely@hotmail.fr>
2015-10-29 11:48:29 -0400

    Clean code in make_experiment, added automatic saving of this file for each exp

--- Experiments/make_experiment.py
+++ Experiments/make_experiment.py
@@ -1,118 +1,131 @@
 import os, json, random
 from time import gmtime, strftime
 from json import encoder
-encoder.FLOAT_REPR = lambda o: format(o, '.3f')
 
-experimentName = 'Experiment_lists'
 
-def get_json_file_name(layerSizes,
-						graphParameterName=None,
-						graphParameterValue=None,
-						time = True,
-						fileType=".json"):
+
+
+def get_json_file_name(layer_sizes,
+                        graphParameterName=None,
+                        graphParameterValue=None,
+                        fileType=".json",
+                        unique_id=''):
     # Get the name of the file
-	outputFile = 'LayerSize='
-	for l in layerSizes:
-		outputFile +=  str(l) + '_'
-	if graphParameterValue == None:
-		outputFile += '_fc' #Fully connected
-	else:
-		outputFile += graphParameterName + '=' + str(graphParameterValue)
-	outputFile += fileType
-	return(outputFile)
+    outputFile = 'LayerSize='
+    for l in layer_sizes:
+        outputFile +=  str(l) + '_'
+    if graphParameterValue == None:
+        outputFile += '_fc' #Fully connected
+    else:
+        outputFile += graphParameterName + '=' + str(graphParameterValue)
+    outputFile += '_' + unique_id + fileType
+    return(outputFile)
 
-JSONDict = {}
-##General Topology
-layerSizes = [784,300,150,10]
 
-###-----------------Generate Layer Masks
-'''
-Alias for which function to generate the connections for the layer
-Current Options are:
--random Inputs: p
--psuedorect1
--psuedorect2
--psuedosquare1
--psuedosquare2
-'''
+def get_JSON_dict(layer_sizes):
+    ###-----------------Generate Layer Masks
+    '''
+    Alias for which function to generate the connections for the layer
+    Current Options are:
+    -random Inputs: p
+    -psuedorect1
+    -psuedorect2
+    -psuedosquare1
+    -psuedosquare2
+    '''
 
-graphGeneratorAlias = 'random' 
-graphGeneratorParams = {'p' : .6,
-						"layerSizes" : layerSizes} #parameters to be passed to the graph creating function
-seed = random.randint(0,1000) #random esed set to replicate
+    graphGeneratorAlias = 'random'
+    graphGeneratorParams = {'p' : .6,
+                            "layer_sizes" : layer_sizes} #parameters to be passed to the graph creating function
+    seed = random.randint(0,1000) #random esed set to replicate
 
-JSONDict['GenerateLayerMasks'] ={"graphGeneratorAlias" : graphGeneratorAlias,
-								"graphGeneratorParams" : graphGeneratorParams,
-								"seed" : seed}
+    JSON_dict['GenerateLayerMasks'] ={"graphGeneratorAlias" : graphGeneratorAlias,
+                                    "graphGeneratorParams" : graphGeneratorParams,
+                                    "seed" : seed}
 
-###-----------------Create Network
+    ###-----------------Create Network
 
-#Refer to the keras documentation for layer creation
-layerSizes = layerSizes
-layerNames = ["input","L1","L2","output"]
-dropout = 0
-activationFunction= "sigmoid"
-loss="mse"
-lr=0.01
-decay=1e-6
-momentum=0.9
-nesterov=True
-optimizer = "sgd"
-init="glorot_uniform"
+    #Refer to the keras documentation for layer creation
+    layer_sizes = layer_sizes
+    layer_names = ["input","L1","L2","output"]
+    dropout = 0
+    activation_function= "sigmoid"
+    loss="mse"
+    lr=0.01
+    decay=1e-6
+    momentum=0.9
+    nesterov=True
+    optimizer = "sgd"
+    init="glorot_uniform"
 
-JSONDict["CreateNetwork"] = {"layerSizes" : layerSizes,
-							"layerNames" : layerNames,
-							"dropout" : dropout,
-							"activationFunction": activationFunction,
-							"loss": loss, 
-							"lr":lr, 
-							"decay":decay, 
-							"momentum":momentum, 
-							"nesterov":nesterov,
-							"optimizer" : optimizer,
-							"init": init}
+    JSON_dict["CreateNetwork"] = {"layer_sizes" : layer_sizes,
+                                "layer_names" : layer_names,
+                                "dropout" : dropout,
+                                "activation_function": activation_function,
+                                "loss": loss,
+                                "lr":lr,
+                                "decay":decay,
+                                "momentum":momentum,
+                                "nesterov":nesterov,
+                                "optimizer" : optimizer,
+                                "init": init}
 
-####--------------------------------Fit Network
+    ####--------------------------------Fit Network
 
-#Refer to Keras documentation
-nb_epoch = 20
-batch_size = 16 
-validation_split = 0
-show_accuracy =True
-verbose = 'patal'
-output_filepath= 'output_filepath.txt'
+    #Refer to Keras documentation
+    nb_epoch = 20
+    batch_size = 16
+    validation_split = 0
+    show_accuracy =True
+    verbose = 'patal'
+    output_filepath= 'output_filepath.txt'
 
-JSONDict['FitNetwork'] = {"nb_epoch":nb_epoch, 
-						  "batch_size":batch_size, 
-						  "validation_split":validation_split, 
-						  "show_accuracy":show_accuracy, 
-						  "verbose":verbose,
-						  "output_filepath":output_filepath}
+    JSON_dict['FitNetwork'] = {"nb_epoch":nb_epoch,
+                              "batch_size":batch_size,
+                              "validation_split":validation_split,
+                              "show_accuracy":show_accuracy,
+                              "verbose":verbose,
+                              "output_filepath":output_filepath}
+    return(JSON_dict)
 
 ##THE ABOVE ARE STATIC SETTINGS NOW DYNAMIC ONES GO BELOW
-try:
-	os.mkdir(experimentName)
-except:
-	print("Experiment Already Exists")
-#Params to vary
-#p_vals = list(float(x)/100. for x in range(1,31))
-numberOfvalues = 100
-rangeOfValues = range(0,numberOfvalues)
-p_val_input = [1 for i in rangeOfValues]
-p_val_layer_1 = [.5 for i in rangeOfValues]
-p_val_layer_2 = [float(x)/100. for x in rangeOfValues]
-p_val_output = [1 for i in rangeOfValues]
 
-p_vals = [list(i) for i in zip(p_val_input,p_val_layer_1,p_val_layer_2,p_val_output)]
-print p_vals
-for p in p_vals:
-	JSONDict['GenerateLayerMasks']['graphGeneratorParams']['p'] = p
-	#Set seed as well
-	JSONDict['GenerateLayerMasks']['seed'] = random.randint(0,1000)
-	#and the output file name
-	output_filepath = get_json_file_name(layerSizes, "p", p, '.csv')
-	JSONDict['FitNetwork']['output_filepath'] = output_filepath
+if __name__ == "__main__":
+    encoder.FLOAT_REPR = lambda o: format(o, '.3f')
+    unique_id = str(strftime("%Y%m%d_%Hh%Mm%Ss", gmtime()))
+    experiment_name = 'Experiment_'  + unique_id
+    os.mkdir(experiment_name)
+    JSON_dict = {}
 
-	outputFileJSON = get_json_file_name(layerSizes, "p", p)
-	with open(os.path.join(experimentName, outputFileJSON), 'w') as outfile :
-		json.dump(JSONDict, outfile)
+    number_of_values = 90
+    initial_layer_size = 100
+    initial_proba = 1
+    for k in range(number_of_values+1):
+
+        ##General Topology
+        moving_layer_size = initial_layer_size + 10.*k
+        layer_sizes = [784, 300, moving_layer_size, 10]
+        JSON_dict = get_JSON_dict(layer_sizes)
+
+        moving_proba = initial_layer_size / moving_layer_size * initial_proba
+        moving_probas = [1, 1, moving_proba, 1]
+        p = moving_probas
+
+        JSON_dict['GenerateLayerMasks']['graphGeneratorParams']['p'] = p
+        #Set seed as well
+        JSON_dict['GenerateLayerMasks']['seed'] = random.randint(0,1000)
+        #and the output file name
+        output_filepath = get_json_file_name(layer_sizes, "p", p, fileType='.csv', unique_id=unique_id)
+        JSON_dict['FitNetwork']['output_filepath'] = output_filepath
+
+        output_file_JSON = get_json_file_name(layer_sizes, "p", p, unique_id=unique_id)
+        file = open(os.path.join(experiment_name, output_file_JSON), 'w')
+        json.dump(JSON_dict, file)
+        file.close()
+
+    # Save this JSON creator file
+    saved_make_exp = open(os.path.join(experiment_name, 'make_experiment.py'), 'w')
+    make_exp_file = open('make_experiment.py', 'r')
+    saved_make_exp.write(make_exp_file.read())
+    make_exp_file.close()
+    saved_make_exp.close()
